7dce48fba8daa585cf196eff71ae67ee8fc4745d
Warrper <WarrenPerkins@mail.com>
2018-12-11 04:02:33 +0000

    Fixed Crashing when an invalid title is enterd

--- APIRequst.py
+++ APIRequst.py
@@ -1,25 +1,29 @@
 import requests
 import json
 
-movieTitle = input("Please input a movie title\n> ")
+def Movie_Search():
+  movieTitle = input("Please input a movie title\n> ")
 
+  reqURL = 'http://www.omdbapi.com/?t=' + movieTitle.replace(" ", "+") + "&apikey=464fd173"
 
-reqURL = 'http://www.omdbapi.com/?t=' + movieTitle.replace(" ", "+") + "&apikey=464fd173"
+  r = requests.get(reqURL)
+  rText = r.text
+  rDict = json.loads(rText)
 
-r = requests.get(reqURL)
-rText = r.text
-rDict = json.loads(rText)
-
-print("Title: " + rDict["Title"])
-print("Year: " + rDict["Year"])
-print("Rated: " + rDict["Rated"])
-print("Released: " + rDict["Released"])
-print("Runtime: " + rDict["Runtime"])
-print("Genre: " + rDict["Genre"])
-print("Director: " + rDict["Director"])
-print("Actors: " + rDict["Actors"])
-print("Plot: " + rDict["Plot"])
-print("Language: " + rDict["Language"])
-print("Country: " + rDict["Country"])
-print("Awards: " + rDict["Awards"])
-
+  try:
+    print("Title: " + rDict["Title"])
+    print("Year: " + rDict["Year"])
+    print("Rated: " + rDict["Rated"])
+    print("Released: " + rDict["Released"])
+    print("Runtime: " + rDict["Runtime"])
+    print("Genre: " + rDict["Genre"])
+    print("Director: " + rDict["Director"])
+    print("Actors: " + rDict["Actors"])
+    print("Plot: " + rDict["Plot"])
+    print("Language: " + rDict["Language"])
+    print("Country: " + rDict["Country"])
+    print("Awards: " + rDict["Awards"])
+  except:
+    print("Please enter a valid move title\n")
+    Movie_Search()
+Movie_Search()
